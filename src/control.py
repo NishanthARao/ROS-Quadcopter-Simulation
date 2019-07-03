fa54c6bf8316a81562b3241b74467c9394fc7bec
Nishanth Rao <nishantharao@gmail.com>
2019-07-03 16:32:10 +0530

    Add files via upload

---
+++ src/control.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#---------------------------------------------------
+from pid import PID
+import rospy
+from gazebo_msgs.msg import ModelStates
+from std_msgs.msg import Float64MultiArray, Float32
+from geometry_msgs.msg import Pose
+from tf.transformations import euler_from_quaternion
+#---------------------------------------------------
+def control_kwad(msg, args):
+	#Declare global variables as you dont want these to die, reset to zero and then re-initiate when the function is called again.
+	global roll, pitch, yaw, err_roll, err_pitch, err_yaw
+	
+	#Assign the Float64MultiArray object to 'f' as we will have to send data of motor velocities to gazebo in this format
+	f = Float64MultiArray()
+	
+	#Convert the quaternion data to roll, pitch, yaw data
+	#The model_states contains the position, orientation, velocities of all objects in gazebo. In the simulation, there are 3 objects: ground, Contruction_cone, and the quadcopter. So 'msg.pose[2]' will access the 3rd object's pose information i.e the quadcopter's pose.
+	orientationObj = msg.pose[2].orientation
+	orientationList = [orientationObj.x, orientationObj.y, orientationObj.z, orientationObj.w]
+	(roll, pitch, yaw) = (euler_from_quaternion(orientationList))
+	
+	#send roll, pitch, yaw data to PID() for attitude-stabilisation, along with 'f', to obtain 'fUpdated'
+	#Alternatively, you can add your 'control-file' with other algorithms such as Reinforcement learning, and import the main function here instead of PID().
+	(fUpdated, err_roll, err_pitch, err_yaw) = PID(roll, pitch, yaw, f)
+	
+	#The object args contains the tuple of objects (velPub, err_rollPub, err_pitchPub, err_yawPub. publish the information to namespace.
+	args[0].publish(fUpdated)
+	args[1].publish(err_roll)
+	args[2].publish(err_pitch)
+	args[3].publish(err_yaw)
+	#print("Roll: ",roll*(180/3.141592653),"Pitch: ", pitch*(180/3.141592653),"Yaw: ", yaw*(180/3.141592653))
+	#print(orientationObj)
+#----------------------------------------------------
+
+#Initiate the node that will control the gazebo model
+rospy.init_node("Control")
+
+#initiate publishers that publish errors (roll, pitch,yaw - setpoint) so that it can be plotted via rqt_plot /err_<name>  
+err_rollPub = rospy.Publisher('err_roll', Float32, queue_size=1)
+err_pitchPub = rospy.Publisher('err_pitch', Float32, queue_size=1)
+err_yawPub = rospy.Publisher('err_yaw', Float32, queue_size=1)
+
+#initialte publisher velPub that will publish the velocities of individual BLDC motors
+velPub = rospy.Publisher('/Kwad/joint_motor_controller/command', Float64MultiArray, queue_size=4)
+
+#Subscribe to /gazebo/model_states to obtain the pose in quaternion form
+#Upon receiveing the messages, the objects msg, velPub, err_rollPub, err_pitchPub and err_yawPub are sent to "control_kwad" function.
+PoseSub = rospy.Subscriber('/gazebo/model_states',ModelStates,control_kwad,(velPub, err_rollPub, err_pitchPub, err_yawPub))
+
+rospy.spin()
---
+++ src/pid.py
@@ -0,0 +1,163 @@
+#-------------
+import time
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+def PID(roll, pitch, yaw, f):
+	#Define the global variables to prevent them from dying and resetting to zero, each time a function call occurs. Some of these variables 		may be redundant.
+	global kp_roll, ki_roll, kd_roll, kp_pitch, ki_pitch, kd_pitch, kp_yaw, ki_yaw, kd_yaw, prevErr_roll, prevErr_pitch, prevErr_yaw, pMem_roll, pMem_yaw, pMem_pitch, iMem_roll, iMem_pitch, iMem_yaw, dMem_roll, dMem_pitch, dMem_yaw, flag, setpoint, sampleTime
+	#-----------------------
+	#Assign your PID values here. From symmetry, control for roll and pitch is the same.
+	kp_roll = 10
+	ki_roll = 0.0002
+	kd_roll = 3.8
+	kp_pitch = kp_roll
+	ki_pitch = ki_roll
+	kd_pitch = kd_roll
+	kp_yaw = 0.1
+	ki_yaw = 0
+	kd_yaw = 0
+	flag = 0
+	#Define other variables here, and calculate the errors.
+	sampleTime = 0
+	setpoint = 0
+	err_pitch = float(pitch)*(180 / 3.141592653) - setpoint 
+ 	err_roll = float(roll)*(180 / 3.141592653) - setpoint
+	err_yaw = float(yaw)*(180/3.14159263) - setpoint
+	currTime = time.time()
+	#-----------------------
+	#Reset the following variables during the first run only.
+	if flag == 0:
+		prevTime = 0
+		prevErr_roll = 0
+		prevErr_pitch = 0
+		prevErr_yaw = 0
+		pMem_roll = 0
+		pMem_pitch = 0
+		pMem_yaw = 0
+		iMem_roll = 0
+		iMem_pitch = 0
+		iMem_yaw = 0
+		dMem_roll = 0
+		dMem_pitch = 0
+		dMem_yaw = 0
+		flag += 1
+	#------------------------
+	#Define dt, dy(t) here for kd calculations.
+	dTime = currTime - prevTime
+	dErr_pitch = err_pitch - prevErr_pitch
+	dErr_roll = err_roll - prevErr_roll
+	dErr_yaw = err_yaw - prevErr_yaw
+	
+	#-------------------------------------------------------------------------------------------------------------------------------
+	#This is the Heart of the PID algorithm. PID behaves more accurately, if it is sampled at regular intervals. You can change the sampleTime to whatever value is suitable for your plant.
+	if(dTime >= sampleTime):
+		#Kp*e(t)
+		pMem_roll = kp_roll * err_roll
+		pMem_pitch = kp_pitch * err_pitch
+		pMem_yaw = kp_yaw * err_yaw
+		
+		iMem_roll += err_pitch * dTime
+		iMem_pitch += err_roll * dTime
+		iMem_yaw += err_yaw * dTime
+		
+		#integral(e(t))
+		if(iMem_roll > 400): iMem_roll = 400
+		if(iMem_roll < -400): iMem_roll = -400
+		if(iMem_pitch > 400): iMem_pitch = 400
+		if(iMem_pitch < -400): iMem_pitch = -400
+		if(iMem_yaw > 400): iMem_yaw = 400
+		if(iMem_yaw < -400): iMem_yaw = 400
+		
+		#derivative(e(t))
+		dMem_roll = dErr_roll / dTime
+		dMem_pitch = dErr_pitch / dTime
+		dMem_yaw = dErr_yaw / dTime
+	
+	#Store the current variables into previous variables for the next iteration.
+	prevTime = currTime
+	prevErr_roll = err_roll
+	prevErr_pitch = err_pitch
+	prevErr_yaw = err_yaw
+	
+	#output = Kp*e(t) + Ki*integral(e(t)) + Kd*derivative(e(t))
+	output_roll = pMem_roll + ki_roll * iMem_roll + kd_roll * dMem_roll
+	output_pitch = pMem_pitch + ki_pitch * iMem_pitch + kd_pitch * dMem_pitch
+	output_yaw = pMem_yaw + ki_yaw * iMem_yaw + kd_yaw * dMem_yaw 
+	#-------------------------------------------------------------------------------------------------------------------------------
+		#Ignore this.
+	#br_motor_vel = 50.5 + output_pitch + output_roll + output_yaw
+	#bl_motor_vel = 50.5 - output_pitch + output_roll - output_yaw
+	#fl_motor_vel = 50.5 - output_pitch - output_roll + output_yaw
+	#fr_motor_vel = 50.5 + output_pitch - output_roll - output_yaw
+	
+	#-------------------------------------------------------------------------------------------------------------------------------
+	#Some Gazebo information for your reference.
+	
+	#Positive roll is right wing down
+	#Positive pitch is front nose down
+	#Positive yaw is rotate CCW about z-axis
+	
+	#Red is x-axis
+	#Green is y-axis
+	#Blue is z-axis
+	
+	#-------------------------------------------------------------------------------------------------------------------------------
+	#br: Back Right
+	#bl: Back Left
+	#fl: Front Left
+	#fr: Front Right
+	#Calculate the ESC pulses (1000us - 2000us PWM signal) for each of the motor.
+	
+	#br in my code is fr in gazebo's world
+	esc_br = 1500 + output_roll + output_pitch - output_yaw
+	#bl in my code is br in gazebo's world
+	esc_bl = 1500 + output_roll - output_pitch + output_yaw
+	#fl in my code is bl in gazebo's world
+	esc_fl = 1500 - output_roll - output_pitch - output_yaw
+	#fr in my code is fl in gazebo's world
+	esc_fr = 1500 - output_roll + output_pitch + output_yaw
+	
+	#Limit the ESC pulses to upper limit and lower limit, in case the PID algorithm goes crazy and high af.
+	if(esc_br > 2000): esc_br = 2000
+	if(esc_bl > 2000): esc_bl = 2000
+	if(esc_fr > 2000): esc_fr = 2000
+	if(esc_fl > 2000): esc_fl = 2000
+	
+	if(esc_br < 1100): esc_br = 1100
+	if(esc_bl < 1100): esc_bl = 1100
+	if(esc_fr < 1100): esc_fr = 1100
+	if(esc_fl < 1100): esc_fl = 1100
+	
+	#Map the esc values to motor values
+	br_motor_vel = ((esc_br - 1500)/25) + 50
+	bl_motor_vel = ((esc_bl - 1500)/25) + 50
+	fr_motor_vel = ((esc_fr - 1500)/25) + 50
+	fl_motor_vel = ((esc_fl - 1500)/25) + 50
+	#----------------------------------------------------------------------------------------------------------------------------------
+	#Ignore this shit here.
+	'''
+	if(fl_motor_vel > 70): fl_motor_vel = 70
+	if(fr_motor_vel > 70): fr_motor_vel = 70
+	if(bl_motor_vel > 70): bl_motor_vel = 70
+	if(br_motor_vel > 70): br_motor_vel = 70
+	
+	
+	if(err_roll > 0 && err_pitch > 0):
+		fl_motor_vel = 51
+		fr_motor_vel = 45
+		bl_motor_vel = 51
+		br_motor_vel = 51
+	elif(err_roll > 0 && err_pitch < 0):
+		fl_motor_vel = 45
+		fr_motor_vel = 51
+		bl_motor_vel = 51
+		br_motor_vel = 51
+	elif(err_roll < 0 && err_pitch > 0):
+	'''	
+	#---------------------------------------------------------------------------------------------------------------------------------
+	#Provide the motor velocities to the object 'f' that will now exit out of this function, and gets published to gazebo, providing velocities to each motor. Note that the sign here is +,-,+,- i.e CW, CCW, CW, CCW in gazebo model. Change view of gazebo model (by scrolling) such that the green line comes to your left, red line goes forward, and blue line goes upward. This is the convention that i refer to as "Gazebo model" incase you get confused.
+	f.data = [fr_motor_vel,-fl_motor_vel,bl_motor_vel, -br_motor_vel]
+	
+	#Return these variables back to the control file.
+	return f, err_roll, err_pitch, err_yaw
+#--------------------------------------------------------------------------------------------------------------------------------------
